89fee59368aa82027900ce47d62c787d0e1ad5ab
Oluebube5 <100016422+Oluebube5@users.noreply.github.com>
2023-03-30 18:03:15 +0100

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,12 @@
-import updateUniqueItems from "./10-update_uniq_items.js";
-import groceriesList from "./9-groceries_list.js";
-
-const map = groceriesList();
-console.log(map);
-
-updateUniqueItems(map)
-console.log(map);
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    for (const [key, value] of map) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    }
+  } else {
+    throw new Error('Cannot process');
+  }
+  return map;
+}
